31080f71fa16e7b81d649ec17febf195cbe84c5c
kovacs.marton2 <kovacs.marton2@C19-09>
2025-04-07 08:16:18 +0200

    Add project files.

---
+++ WF_15/FileManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+
+namespace WF_15
+{
+    class FileManager
+    {
+        private string filename;
+        public FileManager(string FileIn)
+        {
+            filename = FileIn;
+        }
+        public List<Person> ReadFile()
+        {
+            List<Person> returnlist = new List<Person>();
+            try
+            {
+                StreamReader r = new StreamReader(filename);
+                r.ReadLine();
+                while (!r.EndOfStream)
+                {
+                    returnlist.Add(new Person(r.ReadLine()));
+                }
+                r.Close();
+            }
+            catch (Exception e) { MessageBox.Show(e.Message, "Error message"); }
+            return returnlist;
+        }
+        public void WriteFile(List<Person> all)
+        {
+            try
+            {
+                StreamWriter w = new StreamWriter(filename, false, Encoding.UTF8);
+                w.WriteLine("elsosor");
+                foreach (Person p in all)
+                {
+                    w.WriteLine(p.Weight + "-" + p.Height);
+                }
+                w.Close();
+            }
+            catch (Exception e) { MessageBox.Show(e.Message, "Error message"); }
+        }
+    }
+}
---
+++ WF_15/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WF_15
+{
+    class Person
+    {
+        public int Height { get; set; }
+        public int Weight { get; set; }
+        public Person(string line)
+        {
+            string[] s = line.Split('-');
+            Height = int.Parse(s[0]);
+            Weight = int.Parse(s[1]);
+        }
+        public Person(int weight, int height)
+        {
+            Height = height;
+            Weight = weight;
+        }
+        public Person()
+        {
+            Weight = 25;
+            Height = 195;
+        }
+    }
+}
